9be8447f264a72d23c18962eb8fbe00f932da62d
SushanPoojary <sushanpoojary@gmail.com>
2022-05-25 20:28:07 -0700

    env variables

--- src/env.js
+++ src/env.js
@@ -1,1 +1,1 @@
-export const URL = 'http://cec275-env.eba-dx63agxy.us-east-2.elasticbeanstalk.com';
+export const URL = 'https://cors-everywhere.herokuapp.com/http://cec275-env.eba-dx63agxy.us-east-2.elasticbeanstalk.com';
