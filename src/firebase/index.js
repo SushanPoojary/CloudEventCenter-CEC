849bb95be890be4df2f56bc61f109b01a401e98a
Sushan Poojary <sushanpoojary@gmail.com>
2022-06-03 11:00:31 -0700

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBcRdAbmB_N2z_OSylkr3fQqXkjyjmaiLc",
-  authDomain: "cec275-3.firebaseapp.com",
-  projectId: "cec275-3",
-  storageBucket: "cec275-3.appspot.com",
-  messagingSenderId: "751410396654",
-  appId: "1:751410396654:web:064cc5730b34e62869a2ff",
-  measurementId: "G-T6ZD4743T8"
+  apiKey: "AiLc",
+  authDomain: "ceom",
+  projectId: "ec",
+  storageBucket: "cecom",
+  messagingSenderId: "754",
+  appId: "1f",
+  measurementId: "T8"
 };
 
 export const app = initializeApp(firebaseConfig);
